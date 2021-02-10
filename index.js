00c01ccccf2868eb833475655f8b9688310375b2
João Pablo <jp.pablolima@gmail.com>
2021-02-10 12:13:38 -0300

    feat: connection configuration in the database

--- index.js
+++ index.js
@@ -1,13 +1,29 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const connection = require('./database/connection')
 
 PORT = 3000;
 
 
+
 app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+
+
+// Connection in DataBase
+
+connection
+    .authenticate()
+    .then(() => {
+        console.log('connection successful');
+    }).catch((error) => {
+        console.log(error)
+    });
 
 
 app.get('/', (req, res) => {
