bbfc47edad973265a245f636d0dd8c08cf38d8a8
João Pablo <jp.pablolima@gmail.com>
2021-02-10 11:37:26 -0300

    feat: bodyparser configuration and load view engine - ejs

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 const express = require('express');
-
 const app = express();
+const bodyParser = require('body-parser');
 
 PORT = 3000;
 
+
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+
 app.get('/', (req, res) => {
-    res.send('Server running in port 3000');
+    res.render('index');
 });
 
 
