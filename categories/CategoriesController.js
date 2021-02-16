d3596a8112a783d3e882794c94db2beb35568d79
João Pablo <jp.pablolima@gmail.com>
2021-02-16 11:15:45 -0300

    feat: taking data from Category Registration and saving it in the database

--- categories/CategoriesController.js
+++ categories/CategoriesController.js
@@ -1,9 +1,29 @@
 const express = require('express');
 const router = express.Router();
+const Category = require('./Catergory');
+const slugify = require('slugify');
 
 
 router.get('/admin/categories/new', (req, res) => {
     res.render('admin/categories/new')
 });
+
+
+router.post('/categories/save', (req, res) => {
+    let title = req.body.title;
+    if (title != undefined) {
+
+        Category.create({
+            title: title,
+            slug: slugify(title)
+        }).then(() => {
+            res.redirect('/')
+        })
+    } else {
+        res.redirect('/admin/categories/new')
+    }
+})
+
+
 
 module.exports = router;
