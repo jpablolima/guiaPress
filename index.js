371e75369c294caf0d0af07f896dde8b7df39608
João Pablo <jp.pablolima@gmail.com>
2021-02-22 11:36:34 -0300

    feat: category editing

--- index.js
+++ index.js
@@ -1,43 +1,43 @@
-const express = require('express');
-const app = express();
-const bodyParser = require('body-parser');
-const connection = require('./database/connection');
-const categoriesController = require('./categories/CategoriesController');
-const articlesController = require('./articles/ArticleController');
-const Article = require('./articles/Article');
-const Category = require('./categories/Catergory');
+ const express = require('express');
+ const app = express();
+ const bodyParser = require('body-parser');
+ const connection = require('./database/connection');
+ const categoriesController = require('./categories/CategoriesController');
+ const articlesController = require('./articles/ArticleController');
+ const Article = require('./articles/Article');
+ const Category = require('./categories/Catergory');
 
-PORT = 3000;
+ PORT = 3000;
 
-app.set('view engine', 'ejs');
-app.use(express.static('public'));
+ app.set('view engine', 'ejs');
+ app.use(express.static('public'));
 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-
-
-
-// Connection in DataBase
-
-connection
-    .authenticate()
-    .then(() => {
-        console.log('connection successful');
-    }).catch((error) => {
-        console.log(error)
-    });
-
-
-app.use('/', categoriesController);
-app.use('/', articlesController);
+ app.use(bodyParser.urlencoded({ extended: false }));
+ app.use(bodyParser.json());
 
 
 
-app.get('/', (req, res) => {
-    res.render('index');
-});
+ // Connection in DataBase
+
+ connection
+     .authenticate()
+     .then(() => {
+         console.log('connection successful');
+     }).catch((error) => {
+         console.log(error)
+     });
 
 
-app.listen(PORT, () => {
-    console.log(`Server running in port ${PORT}`)
-});
+ app.use('/', categoriesController);
+ app.use('/', articlesController);
+
+
+
+ app.get('/', (req, res) => {
+     res.render('index');
+ });
+
+
+ app.listen(PORT, () => {
+     console.log(`Server running in port ${PORT}`)
+ });
